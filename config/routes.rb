8627833f63f6891a665d8d8c02ec0200ff3e3c0b
princebatra2315 <princebatra2315@gmail.com>
2018-02-21 20:42:58 +0530

    User Profile Created

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/index'
+
+  devise_for :users,  :controllers => { :shows => "devise/my_devise/shows"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: 'pages#index'
+match '/users',   to: 'users#index',   via: 'get'
 post '/profile' , to: 'profiles#create'
 get  '/profile' ,  to: 'profiles#new'
 get  '/find'    ,   to: 'profiles#show'
+#put  '/show'   ,  to: 'shows#show'
 resources :profiles,  only: [:create, :destroy]
 end
