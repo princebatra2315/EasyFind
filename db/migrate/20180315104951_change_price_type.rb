2a7b81dd967bcd0d54dd70efcdbd0377b9fc97e3
princebatra2315 <princebatra2315@gmail.com>
2018-03-15 18:54:58 +0530

    migration bug fixed

--- db/migrate/20180315104951_change_price_type.rb
+++ db/migrate/20180315104951_change_price_type.rb
@@ -1,5 +1,5 @@
 class ChangePriceType < ActiveRecord::Migration[5.0]
   def change
-  	change_column :profiles, :price, :integer
+  	change_column :profiles, :price, 'integer USING CAST(price AS integer)'
   end
 end
