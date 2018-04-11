7362c79055055934eb8009b177d708985d9e91e6
princebatra2315 <princebatra2315@gmail.com>
2018-04-11 18:53:56 +0530

    Authentication Bug Fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ ActionMailer::Base.smtp_settings = {
     :port           => '25',
     :authentication => 'plain',
     :user_name      => 'princebatra2315@gmail.com',
-    :password       => 'neha@1998',
+    :password       => 'batraprince@',
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
 }
