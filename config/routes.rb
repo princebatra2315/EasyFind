4e17e32f98a20b0a39e7a43b43e4eb59aa7fc798
princebatra2315 <princebatra2315@gmail.com>
2018-04-20 00:17:34 +0530

    Frontend Completely Changed

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
   match '/users',    to: 'users#index',   via: 'get'
+  get   '/'     ,    to: 'pages#index'
   post '/profile' ,  to: 'profiles#create'
   get  '/profile' ,  to: 'profiles#new'
   get  '/profiles' ,   to: 'profiles#index'
