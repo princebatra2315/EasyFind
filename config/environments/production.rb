36663ec398b34165206c0bc457531b1c76cdebf0
princebatra2315 <princebatra2315@gmail.com>
2018-04-20 10:28:58 +0530

    Password Changed For email sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ ActionMailer::Base.smtp_settings = {
     :port           => '25',
     :authentication => 'plain',
     :user_name      => 'princebatra2315@gmail.com',
-    :password       => 'batraprince@',
+    :password       => 'nehabatrarohit',
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
 }
