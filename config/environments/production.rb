25c80b5bcd01976e7097cd374db24b16d46b362c
Prince Batra <princebatra2315@gmail.com>
2019-04-24 16:00:48 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   #
 
 config.action_mailer.delivery_method = :smtp
-config.action_mailer.default_url_options = { host: 'easyfind.herokuapp.com' }
+config.action_mailer.default_url_options = { host: 'easy-find.herokuapp.com' }
 
 ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.gmail.com',
